ccd549dc6eded0ed38b426cee729be653e83a1a1
dalictn <78489984+dalictn@users.noreply.github.com>
2024-10-13 18:23:22 +0200

    create func to strip playlist vec of songs/

--- src/main.rs
+++ src/main.rs
@@ -4,17 +4,30 @@ use std::fs::File;
 use std::io;
 use std::io::BufReader;
 use std::io::*;
+use std::ops::Index;
+use std::{
+    fs::read_dir,
+    path::{Path, PathBuf},
+};
+use termion::clear;
 use termion::event::Key;
 use termion::input::TermRead;
 use termion::raw::{IntoRawMode, RawTerminal};
-use termion::clear;
-use std::{fs::read_dir, path::{Path, PathBuf}};
 
 fn rem_last(value: &str) -> &str {
     let mut chars = value.chars();
     chars.next_back();
     chars.as_str()
 }
+
+fn strip_playlist(mut list: Vec<String>) -> Vec<String> {
+    for mut item in list.iter_mut() {
+    //item.to_string().replace(".mp3", "");
+    *item = item.replace("songs/", "").to_string();
+        }
+    list
+}
+
 
 fn idle(mut active: bool) {
     // choosing music file input
@@ -29,122 +42,127 @@ fn idle(mut active: bool) {
     let mut stdout = stdout().into_raw_mode().unwrap();
     writeln!(stdout, "{}{}", clear::All, termion::cursor::Goto(1, 1)).unwrap();
 
-
-    writeln!(stdout , "[o] load ../songs/\n [s] load track").unwrap();
+    writeln!(stdout, "[o] load ../songs/\n [s] load track").unwrap();
     //writeln!(stdout, "[s] load track").unwrap();
+    let mut paused = false;
 
     for c in std::io::stdin().keys() {
-
-        //i reckon this speaks for itself
         match c.unwrap() {
-            Key::Char('o') => {println!("{}", clear::All); play_songs(&sink, active, path);break;},
-
+            Key::Char('o') => {
+                println!("{}", clear::All);
+                play_songs(&sink, active, path, paused);
+                break;
+            }
             Key::Ctrl('q') => panic!(),
-
-            //figure out how to go back to main func from func
-            Key::Char('s') => {println!("{}", clear::All);stdout.suspend_raw_mode().unwrap();play_song(&sink, &active, &mut stdout);break;},
+            Key::Char('s') => {
+                println!("{}", clear::All);
+                stdout.suspend_raw_mode().unwrap();
+                play_song(&sink, &active, &mut stdout, &paused);
+                break;
+            }
             _ => (),
-        }}
-
+        }
+    }
 
     // Load a sound from a file, using a path relative to Cargo.toml
     let mut trimmed_input = input.to_string().trim().to_string();
     let mut playing_song: bool = false;
-
 }
 
-fn play_song(sink: &Sink, mut active: &bool, stdout: &mut RawTerminal<Stdout>) {
-
-
-
+fn play_song(sink: &Sink, mut active: &bool, stdout: &mut RawTerminal<Stdout>, mut paused: &bool) {
     let mut track = String::new().to_string();
     println!("Please enter file name: songs/...");
     let untrimmed_track = io::stdin().read_line(&mut track).unwrap();
     let track = rem_last(&track);
     //track.to_string().trim().to_string();
-    let song = File::open(format!("songs/{track}.mp3")).expect("Could not open song file. Perhaps the filename was misspelt?");
+    let song = File::open(format!("songs/{track}.mp3"))
+        .expect("Could not open song file. Perhaps the filename was misspelt?");
     let source = Decoder::new(song).unwrap();
-
-
 
     // append sound to sink and play
 
     sink.append(source);
 
-    let mut paused = false;
-
     let mut sink_state = sink.empty();
 
-        println!("Song is now playing.");
-        println!("Press K to pause and resume, Ctrl + q to quit.");
-        stdout.flush().unwrap();
-        stdout.activate_raw_mode().unwrap();
+    println!("Song is now playing.");
+    println!("Press K to pause and resume, Ctrl + q to quit.");
+    stdout.flush().unwrap();
+    stdout.activate_raw_mode().unwrap();
 
-        for c in stdin().keys() {
-
-            match c.unwrap() {
-                Key::Char('k') => {
-                    if paused == true {
-                        sink.pause();
-                        paused = false;
-                    } else {
-                        sink.play();
-                        paused = true;
-                    }
-                },
-                Key::Ctrl('q') => panic!(),
-
-                Key::Ctrl('x') => {println!("{}", clear::All); active = &true;return;},
-                _ => (),
+    for c in stdin().keys() {
+        match c.unwrap() {
+            Key::Char('k') => {
+                if paused == &true {
+                    sink.pause();
+                    paused = &false;
+                } else {
+                    sink.play();
+                    paused = &true;
+                }
             }
+            Key::Ctrl('q') => panic!(),
+
+            Key::Ctrl('x') => {
+                println!("{}", clear::All);
+                active = &true;
+                return;
+            }
+            _ => (),
         }
-
     }
-fn play_songs(sink: &Sink, mut active: bool, path: &str) {
-
+}
+fn play_songs(sink: &Sink, mut active: bool, path: &str, mut paused: bool) {
     let list = recurse_files(path).unwrap();
-
-
+    let mut playlist: Vec<String> = Vec::new();
     for entry in list {
-        let file = File::open(entry).unwrap();
+        let file = File::open(&entry).unwrap();
         let source = Decoder::new(file).unwrap();
         sink.append(source);
+        let mut entry_string = entry.display().to_string();
+        playlist.push(entry_string);
     }
 
-        let mut paused = false;
+    //let playlist = strip_playlist(playlist);
 
-        let stdin = stdin();
-        let mut stdout = stdout().into_raw_mode().unwrap();
-        println!("Song is now playing.");
-        println!("Press K to pause and resume, Ctrl + q to quit.");
-        stdout.flush().unwrap();
+    //for mut item in playlist.iter_mut() {
+        //item.to_string().replace(".mp3", "");
+         //*item = item.replace("songs/", "").to_string();
+    //}
+    let playlist = strip_playlist(playlist);
+    //println!("{:?}", playlist);
 
-        for c in stdin.keys() {
-            match c.unwrap() {
-                Key::Char('k') => {
-                    if paused == true {
-                        sink.pause();
-                        paused = false;
-                    } else {
-                        sink.play();
-                        paused = true;
-                    }
-                },
-                Key::Ctrl('q') => panic!(),
-                Key::Ctrl('n') => sink.skip_one(),
+    let stdin = stdin();
+    let mut stdout = stdout().into_raw_mode().unwrap();
+    println!("Currently queued: ");
+    println!("{:?}", playlist);
+    //println!("{}", sink.queue());
+    println!("Press K to pause and resume, Ctrl + q to quit. Ctrl + n for next song in queue");
+    stdout.flush().unwrap();
 
-
-                Key::Ctrl('x') => {
-                    stdout.suspend_raw_mode().unwrap();
-                    active = true;
-                    return;
-                },
-                _ => (),
+    for c in stdin.keys() {
+        match c.unwrap() {
+            Key::Char('k') => {
+                if paused == true {
+                    sink.pause();
+                    paused = false;
+                } else {
+                    sink.play();
+                    paused = true;
+                }
             }
+            Key::Ctrl('q') => panic!(),
+            Key::Ctrl('n') => sink.skip_one(),
+
+            Key::Ctrl('x') => {
+                stdout.suspend_raw_mode().unwrap();
+                active = true;
+                return;
+            }
+            _ => (),
         }
     }
-
-
+}
 
 fn recurse_files(path: impl AsRef<Path>) -> std::io::Result<Vec<PathBuf>> {
     let mut buf = vec![];
@@ -167,17 +185,10 @@ fn recurse_files(path: impl AsRef<Path>) -> std::io::Result<Vec<PathBuf>> {
     Ok(buf)
 }
 
-
-
-
 fn main() {
     let mut active = true;
-
-
 
     while active == true {
         idle(active);
     }
-
-
 }
